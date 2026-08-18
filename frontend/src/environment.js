fcb7ecd4e5fb4625e23c25f2ad366388145e158b
soham <sohammandal605@gmail.com>
2026-08-18 19:33:11 +0530

    update backend url

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,7 +1,6 @@
-let IS_PROD = false;
+let IS_PROD = true;
 const server = IS_PROD ?
-    "https://apnacollegebackend.onrender.com" :
-
+    "https://nexmeet-1-u769.onrender.com" :
     "http://localhost:8000"
 
 
